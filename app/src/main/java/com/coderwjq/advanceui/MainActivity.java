21328f2c218aad6174b3db1076ea5187e382cffc
wjq <coderwjq@126.com>
2017-11-22 09:11:23 +0800

    初始化项目

---
+++ app/src/main/java/com/coderwjq/advanceui/MainActivity.java
@@ -0,0 +1,14 @@
+package com.coderwjq.advanceui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
